06a52615de426729fa46a4f0b6393459b1df2988
remynder0 <remynderyt@gmail.com>
2025-09-24 10:12:13 +0200

    TD1 dev

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// filepath: c:\Users\Rémy\Documents\pong\tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
