ecc6e1fa396fd27f71dcf4bce13ab294024d1afa
remynder0 <remynderyt@gmail.com>
2025-10-01 21:15:27 +0200

    correction

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// filepath: c:\Users\Rémy\Documents\pong\tailwind.config.js
-module.exports = {
-  content: ["./src/**/*.{html,js}"], // Adjust paths as needed
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
